7bedea209c4a9fd49656592dcf36fa2dbcbd6428
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2023-01-06 15:22:30 -0300

    Adjusts server.ts for new singleton Http Server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
-import HttpServer from './infra/http/HttpServerSingleton';
-import './routes/main.routes';
+import HttpServerSingleton from './infra/http/HttpServerSingleton';
+import ExpressHttpServer from './infra/http/ExpressHttpServer';
+import FastifyHttpServer from './infra/http/FastifyHttpServer';
+import RestifyHttpServer from './infra/http/RestifyHttpServer';
+import MainRoutes from './routes/main.routes';
 
-HttpServer.startServerHttp(3000);
+new HttpServerSingleton(new RestifyHttpServer());
+new MainRoutes();
+
+HttpServerSingleton.getInstance().startServerHttp(3000);
