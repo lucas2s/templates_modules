2b739f6349df7a3ffcbe014db6ef5ac0e4f7b110
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2023-01-06 00:03:37 -0300

    Create Http Server Singleton

---
+++ src/infra/http/HttpServerSingleton.ts
@@ -0,0 +1,7 @@
+import ExpressHttpServer from './ExpressHttpServer';
+import FastifyHttpServer from './FastifyHttpServer';
+import RestifyHttpServer from './RestifyHttpServer';
+
+const HttpServer = new ExpressHttpServer();
+export default HttpServer;
+
